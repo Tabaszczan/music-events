aca362fb329e2fd34d0ecf04fc45a99a405dd1f4
Podolak M <mati.pooo@gmail.com>
2021-01-21 22:46:08 +0100

    test

--- artists/tests/test_models.py
+++ artists/tests/test_models.py
@@ -1,26 +1,27 @@
 from django.test import TestCase
 
+from events.models import Event
 from events.tests.factories import EventFactory
+from ..models import Artist
 from ..tests.factories import ArtistFactory
 
 
 class TestArtist(TestCase):
     """Test Artist model."""
-    def setUp(self) -> None:
-        self.artist = ArtistFactory()
-
 
     def test_string_representation(self):
         """Test if model has valid string representation."""
+        artist = ArtistFactory()
         self.assertEqual(
-            str(self.artist),
-            f'Artist: {self.artist.name}, Genre: {self.artist.genre}'
+            str(artist),
+            f'Artist: {artist.name}, Genre: {artist.genre}'
         )
 
     def test_events(self):
         """Test artist events list."""
-        event = EventFactory(artist=self.artist)
+        artist = ArtistFactory()
+        event = EventFactory(artists__in=artist)
         self.assertEqual(
-            event.values_list('name', flat=True),
-            self.artist.events
+            event.name,
+            Artist.objects.first().events,
         )
